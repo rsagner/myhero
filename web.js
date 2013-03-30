38089c7f7071dcd287da5f06e87f7d34923a3a64
Rikard Sagnér <rikard@metodika.com>
2013-03-30 14:32:05 +0100

    add google

--- web.js
+++ web.js
@@ -11,6 +11,34 @@ app.get('/echo', function(request, response) {
 });
 
 
+app.get('/google', function(request, response) {
+  	http.get("http://www.google.se", function(res){}).on('response',function (feed) {
+
+		response.writeHead(200, {'Content-Type': 'text/html','Accept-Charset': 'utf-8'});
+
+		feed.on('data', function (chunk) {
+
+			response.write(chunk);
+	
+		});
+
+		feed.on('end', function () {
+
+			response.end();
+	
+		});
+
+
+	});
+});
+
+
+
+
+
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
