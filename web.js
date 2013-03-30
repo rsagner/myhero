85d6ea88e5b015145cf536c469f7b1a08a316cde
Rikard Sagnér <rikard@metodika.com>
2013-03-30 14:26:13 +0100

    new url

--- web.js
+++ web.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World! - by Rikard');
 });
 
+app.get('/echo', function(request, response) {
+  response.send('Echo! - by Rikard');
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
