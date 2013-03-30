01e3bd514d5fe3f12a91e42f32dc4987554684c1
Rikard Sagnér <rikard@metodika.com>
2013-03-30 14:12:32 +0100

    changing response

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! - Rikard');
+  response.send('Hello World! - by Rikard');
 });
 
 var port = process.env.PORT || 5000;
