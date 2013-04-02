456565804b903c20301e41cb6638410fef973517
rsagner <rikard@metodika.com>
2013-04-02 22:34:16 +0200

    removed white space from web.js

--- web.js
+++ web.js
@@ -34,13 +34,6 @@ app.get('/google', function(request, response) {
 	});
 });
 
-
-
-
-
-
-
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
